97da54932a26fd3eb938d827e8cd52be25c9e991
Ryan Kelley <kelleyr@duvalschools.org>
2021-06-24 23:10:47 -0400

    Create pair_programming.py

---
+++ pair_programming.py
@@ -0,0 +1,19 @@
+# The student whose birthday is earliest in the year is Programmer #1.  The other student is Programmer #2.  If both students share a birthday, the taller student is Programmer #1.  
+# Programmer #1 and #2 will take turns writing code.  Programmer #1 will start coding.
+# Programmer #1 can write, change, or delete EVEN NUMBERED LINES OF CODE ONLY. 
+# Programmer #2 can write, change, or delete ODD NUMBERED LINES OF CODE ONLY. 
+# LINE NUMBERING WILL CHANGE AS YOU WRITE YOUR CODE SO PAY ATTENTION! 
+# Each programmer can switch their turn at any time.  Sometimes the teacher will tell students to switch.
+# Each programmer must COMMIT AND SUBMIT before switching turns.  
+# Make sure to FETCH any changes when your turn starts. 
+
+
+# The next two lines declare variables named programmer1_name and programmer2_name.  They are assigned string values. Each programmer should update the correct line. 
+programmer1_name = "Type Your Name Here"
+programmer2_name = "Type Your Name Here"
+
+# Programmer #1 edit this comment to indicate your FAVORITE FOOD and NON-ALCOHOLIC DRINK.
+# Programmer #2 edit this comment to indicate your FAVORITE FOOD and NON-ALCOHOLIC DRINK.
+
+
+# The next two lines declare variables named x and y.  
